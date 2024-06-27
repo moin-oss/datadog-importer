7fb0f61ef9cd8bad3830287219ea9351c113895b
Erik Forkin <eforkin@indeed.com>
2024-06-27 18:31:07 -0500

    8-add-unit-tests: Update unit tests to work with UTC

--- src/__tests__/unit/lib/datadog-importer-plugin.test.ts
+++ src/__tests__/unit/lib/datadog-importer-plugin.test.ts
@@ -45,7 +45,7 @@ describe('DatadogImporter(): ', () => {
   const validInput = [
     {
       'instance-id': 'i-123456',
-      timestamp: '2024-06-10T00:00',
+      timestamp: '2024-06-10T05:00:00.000Z',
       duration: 20,
     },
   ];
